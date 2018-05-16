072bbc2ab226531a2db4e320d1ab60dfaafd92ab
Esti Tweg <estictweg@hotmail.com>
2018-05-16 22:05:04 +0930

    Added bouldering climbing schemes

--- GradeConverter/GradeConverter/RoutesViewController.swift
+++ GradeConverter/GradeConverter/RoutesViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class RoutesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITableViewDelegate, UITableViewDataSource {
     
-    var schemes: [String] = ["French", "UK",  "Australia", "UIAA", "North America"]
+    var schemes: [String] = ["French", "UK",  "Australia", "UIAA", "North America", "Hueco", "UK", "Font"]
     
     var grades: [[String]] = [
         ["1-2", "2-3", "4", "4+", "5a", "5b", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b", "7b+", "7c", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+", "9a"],
         ["HVD", "MS", "S", "VS", "HVS", " ", "E1 5b", "E2 5c", " ", "E3 5c", " ", "E4 6a", " ", "E5 6b", " ", "E6 6b", " ", " ", "E7 6c", " ", "E8 7a", " ", "E9 7b", " ", "E10 7c"],
         ["8-9", "10-12", "13-", "13+", "14", "15", "19", "19/20", "20", "21", "21/22", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"],
         ["I-II", "III", "IV", "V-", "V", "V+", "VI+", "VI+/VII-", "VII", "VII+", "VII+/VIII-", "VIII-", "VIII", "VIII/VIII+", "VIII+", "IX-", "IX", "IX/IX+", "IX+", "X-", "X", "X/X+", "X+", "XI-", "XI"],
-        ["5.2-5.3", "5.4-5.5", "5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c/d", "5.12a", "5.12b", "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d", "5.14a", "5.14b", "5.14c", "5.14d-5.15"]]
+        ["5.2-5.3", "5.4-5.5", "5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", "5.11a", "5.11b", "5.11c/d", "5.12a", "5.12b", "5.12c", "5.12d", "5.13a", "5.13b", "5.13c", "5.13d", "5.14a", "5.14b", "5.14c", "5.14d-5.15"],
+        ["","","","","","V0", "","V0+", "V1", "", "V2", "V3", "", "V4", "V5", "V6", "", "V7", "V8", "", "V9", "V10", "V11", "V12/13"],
+        ["","","","","","B1", "","B2", "B3", "", "B4", "", "B5-6", "", "", "B7", "", "B8", "", "B9", "", "B10", "", "B12"],
+        ["","","","","","4", "","4+", "5", "", "6a", "6a+", "", "6b/c", "6c", "6c+", "7a", "7a+", "7b", "7c", "", "7c+", "", "8a+"]]
     
 
     @IBOutlet weak var selectedGradesList: UICollectionView!
